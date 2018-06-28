f926bed8c44dac1b600a66a75753ea316d332301
Ngahu <jamaalaraheem@gmail.com>
2018-06-28 14:57:11 +0300

    finishing up on the post create component .Creating of the submiting of the created post to json placeholdr and loging the new created post to the console .This is the final part of using only react and now introducing redux

--- src/components/PostCreate.js
+++ src/components/PostCreate.js
@@ -9,6 +9,7 @@ class PostCreate extends Component {
             body:''
         };
         this.onChangeInput = this.onChangeInput.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
 
     }
 
@@ -17,6 +18,26 @@ class PostCreate extends Component {
         this.setState({
             [e.target.name]:e.target.value
         });
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+
+        const post = {
+            title:this.state.title,
+            body:this.state.body
+        }
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(post)
+        })
+        .then(res =>res.json())
+        .then(data => console.log(data))
+
     }
 
 
